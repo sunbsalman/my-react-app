f5adde4d99fdee2bb7fe999973a5727b5cbf185b
sunbsalman <sunbsalman@gmail.com>
2024-08-05 10:20:03 +0530

    a

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Todo from './Todo';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const addTodo = () => {
+    if (inputValue.trim()) {
+      setTodos([...todos, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <Todo key={index} todo={todo} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
